a9a228eb923717646dd9fa79bf7b9b940cd1c600
Julia Kramareva <forjobonly100@gmail.com>
2018-07-02 21:02:50 +0300

    new files

---
+++ pr3/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.mob-menu').click(function() {
+	$('.nav-top').slideToggle(500);
+});
+
+     // $('.pic-slide').slick({
+     // 	dots: false,
+     // 	arrows: true,
+     // 	prevArrow: '.arrow1',
+     //    nextArrow: '.arrow2',
+        
+     // });
+    });
